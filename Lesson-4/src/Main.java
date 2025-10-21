bac24be81560d4111077061ebd65708aa1b188b0
TheHashimov <Thehashimov@gmail.com>
2025-10-21 15:58:28 +0400

    Task-4

--- Lesson-4/src/Main.java
+++ Lesson-4/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println(" git");
-        System.out.println();
+        Book b1 = new Book("Clean Code", "Robert C. Martin", "1234567890");
+
+        b1.printInfo();
+        b1.borrowBook();
+        b1.returnBook();
+        Book.libraryRules();
+        b1.bookType();
     }
 }
---
+++ Lesson-5/src/LessonPractice.java
@@ -0,0 +1,7 @@
+public class LessonPractice {
+
+     public void main() {
+         Dog dog = new Dog();
+         System.out.println(Dog.name);
+    }
+}
